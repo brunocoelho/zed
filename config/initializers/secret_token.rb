2f2a510b77402cb44ee394296d01b5fa3c72856d
Rodrigo Alves <rodrigovieira1994@gmail.com>
2013-10-08 14:38:46 -0300

    Beginning Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zed::Application.config.secret_key_base = '2cdb02e9a05f6ccef1e932003a54bdf0cecf70e6fd2921cf00b253dea954e084d9b8c8d015b384a29e3717449fc2c162074a59922ea1c595c68451d598d22a2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zed::Application.config.session_store :cookie_store, key: '_zed_session'
